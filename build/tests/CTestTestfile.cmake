dbaf98f664f9e649da4f28dd661290e684336a69
Ubuntu <ubuntu@localhost.localdomain>
2026-02-27 20:27:37 +0800

    Fix CMake cache issue in CI and add .gitignore

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /app/tests
-# Build directory: /app/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CppChatTests "/app/build/tests/cppchat_tests")
-set_tests_properties(CppChatTests PROPERTIES  _BACKTRACE_TRIPLES "/app/tests/CMakeLists.txt;25;add_test;/app/tests/CMakeLists.txt;0;")
